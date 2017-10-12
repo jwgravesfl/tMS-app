3129cfd2863e10c1542a846edcdf718f7867e616
J. Graves <jwgravesfl@live.com>
2017-10-12 15:34:10 -0400

    App Bar Added to Top.  4 jsx sections created

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+
+import Footer from './Footer';
+import Header from './Header';
+import Page1 from './Page1';
+import Page2 from './Page2';
+
 
 export default class App extends Component {
   render(){
     return (
       <MuiThemeProvider>
-        <RaisedButton label="This is Working"></RaisedButton>
+        <div className="container">
+          <AppBar
+            title="App Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            showMenuIconButton={false} />
+          <div className="row">
+            <div className="col s12 m12"><Header /></div>
+            <div className="col s12 m12"><Page1 /></div>
+            <div className="col s12 m12"><Page2 /></div>
+            <div className="col s12 m12"><Footer /></div>
+          </div>
+        </div>
+
       </MuiThemeProvider>
     )
   }
---
+++ imports/ui/Page1.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Page1 extends Component {
+  render(){
+    return (
+        <h1>Page1</h1>
+    )
+  }
+}
